a83503958244c9bb039ae20a84f455723648842e
79629 <krulikovskayairina@gmail.com>
2024-11-23 22:10:51 +0300

    добавлена кнопка и функция "Остановить музыку"

--- Proect_Napominanie.py
+++ Proect_Napominanie.py
@@ -6,6 +6,7 @@ import time
 import pygame
 
 t = 0
+music = False
 
 def set():
     global t
@@ -16,7 +17,7 @@ def set():
             minute = int(rem.split(":")[1])
             now = datetime.datetime.now()
             print(now)
-            dt = now.replace(hour=hour, minute=minute)
+            dt = now.replace(hour=hour, minute=minute, second=0)
             print(dt)
             t = dt.timestamp()
             print(t)
@@ -36,9 +37,20 @@ def check():
 
 
 def play_snd():
+    global music
+    music = True
     pygame.mixer.init()
     pygame.mixer.music.load("reminder.mp3")
     pygame.mixer.music.play()
+
+
+def stop_music():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music = False
+    label.config(text="Установить новое напоминание")
+
 
 window = Tk()
 window.title("Наименование")
@@ -47,7 +59,10 @@ label = Label(text="Установите напоминание", font=("Arial",
 label.pack(pady=10)
 
 set_button = Button(text='Установить напоминание', command=set)
-set_button.pack()
+set_button.pack(pady=10)
+
+stop_button = Button(text="Остановить музыку", command=stop_music)
+stop_button.pack(pady=10)
 
 check()
 
